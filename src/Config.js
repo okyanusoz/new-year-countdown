cedc317478ed5d1d476f02ec65d2e6c01a177fe6
okyanusoz <46757266+okyanusoz@users.noreply.github.com>
2020-12-31 15:23:58 +0300

    Testing CI

--- src/Config.js
+++ src/Config.js
@@ -1,7 +1,7 @@
 const Config = {
     // countdownDate is the date to countdown to in Unix Epoch seconds
     countdownDate: 1609448400,
-    year: "2021"
+    year: "2021
 }
 
 // You don't have to touch here, it's just for JS to understand the Epoch seconds
