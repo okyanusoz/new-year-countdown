30ecae6daf089e643e7156c7b20b0a3a64c28d74
okyanusoz <46757266+okyanusoz@users.noreply.github.com>
2021-01-03 17:19:09 +0300

    Testing CI

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
+import bootstrap/dist/css/bootstrap.min.css
 import Config from './Config.js'
 import { constructTimeLeftItem } from './TimeLeft.js'
 import { useEffect, useState } from 'react'
