d66548346c18bd03737021cdf84084791fcdda9f
okyanusoz <46757266+okyanusoz@users.noreply.github.com>
2021-03-26 21:46:27 +0300

    Update year to 2022

--- src/Config.js
+++ src/Config.js
@@ -1,6 +1,6 @@
 const Config = {
     // countdownDate is the date to countdown to in JS Date syntax
-    countdownDate: "January 1, 2021 00:00:00",
+    countdownDate: "January 1, 2022 00:00:00",
     // year is the year to display in the UI
     year: "2021",
     // the GitHub repo to redirect to in the Fork me button
