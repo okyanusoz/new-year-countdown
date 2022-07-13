f22dec8d94a91e0c0811e18ad90b164e748f71e0
okyanusoz <46757266+okyanusoz@users.noreply.github.com>
2022-07-13 11:08:13 +0300

    Update Config.js

--- src/Config.js
+++ src/Config.js
@@ -2,7 +2,7 @@ const Config = {
     // countdownDate is the date to countdown to in JS Date syntax
     countdownDate: "January 1, 2023 00:00:00",
     // year is the year to display in the UI
-    year: "2022",
+    year: "2023",
     // githubRepoURL is the GitHub repo URL to redirect to in the Fork me button
     githubRepoURL: "https://github.com/okyanusoz/new-year-countdown",
     // countdownToText is the "Countdown to" text. You can use this for translations.
